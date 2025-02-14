d6fa382734c327fe663e4132bd404dc3c6566a23
Nihal-Ahmad-TTN <nihal.ahmad@tothenew.com>
2025-02-14 11:12:37 +0530

    Add files via upload

---
+++ gethttpcall.py
@@ -0,0 +1,34 @@
+import requests as req
+import json
+
+class getcall:
+    def __init__(self, url,data={}, header='', type='get'):
+        self.url = url
+        self.header = header
+        self.data = data
+        self.type= type
+        self.typecheck()
+        
+    def getdata(self):
+        try:
+            data= req.get(self.url)
+            if data.status_code == 200:
+                json_data= data.json()
+                print(json.dumps(json_data, indent=4))
+        except Exception as e:
+            raise e
+    
+    def typecheck(self):
+        try:
+            if self.type.lower() == "get":
+                self.getdata()
+            else:
+                raise Exception
+        except Exception as e:
+            print(e)
+
+while True:
+    url = input("Enter The URL : ")
+    if url=="" or url ==" ":
+        url = "https://jsonplaceholder.typicode.com/posts"
+    obj = getcall(url)
